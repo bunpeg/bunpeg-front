09b83bdfc95ab41c75d20c6d54087991b1161083
AlejandroYanes <alejandro.yanes94@gmail.com>
2025-06-08 11:27:34 +0100

    chore: fixed build

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -11,7 +11,7 @@ import { initTRPC, TRPCError } from '@trpc/server';
 import superjson from 'superjson';
 import { ZodError } from 'zod';
 
-import { getServerAuthSession } from '@/server/auth';
+// import { auth } from '@/auth';
 
 /**
  * 1. CONTEXT
--- src/trpc/server.ts
+++ src/trpc/server.ts
@@ -6,14 +6,15 @@ import { cache } from 'react';
 
 import { createCaller, type AppRouter } from '@/server/api/root';
 import { createTRPCContext } from '@/server/api/trpc';
+
 import { createQueryClient } from './query-client';
 
 /**
  * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
  * handling a tRPC call from a React Server Component.
  */
-const createContext = cache(() => {
-  const heads = new Headers(headers());
+const createContext = cache(async () => {
+  const heads = new Headers(await headers());
   heads.set('x-trpc-source', 'rsc');
 
   return createTRPCContext({
